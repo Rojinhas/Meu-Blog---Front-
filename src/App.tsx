be53245d0caa32138bdb29db7c117520805d5ca6
Rojinhas <ruthrojasdasilva12@gmail.com>
2024-08-14 13:46:07 -0300

    50.3 - REACT-5 - Prática

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import Home from './pagina/home/Home';
-import Footer from './components/footer/Footer';
+import './App.css';
+
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pagina/login/Login';
 import Cadastro from './pagina/cadastro/Cadastro';
-import { AuthProvider } from './contexts/AuthContext';
+import Home from './pagina/home/Home';
+
 
 function App() {
   return (
-    <AuthProvider>
-    <BrowserRouter>
-    <Navbar />
-    <div className='min-h-[80vh]'>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </div>
-    <Footer />
-  </BrowserRouter>
-  </AuthProvider>
-
-);
+    <>
+   
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+  
+    </>
+  );
 }
 export default App;
