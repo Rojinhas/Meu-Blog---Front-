aa8fd05bf25c12b4ea658f754164555626ca57cf
Rojinhas <ruthrojasdasilva12@gmail.com>
2024-08-14 12:55:16 -0300

    48.2 - REACT-3 - Prática

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import './App.css';
 import Home from './pagina/home/Home';
-import Navbar from './components/footer/Footer';
-import Footer from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pagina/login/Login';
+import Cadastro from './pagina/cadastro/Cadastro';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
-    </>
+    <AuthProvider>
+    <BrowserRouter>
+    <Navbar />
+    <div className='min-h-[80vh]'>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+    <Footer />
+  </BrowserRouter>
+  </AuthProvider>
+
 );
 }
 export default App;
