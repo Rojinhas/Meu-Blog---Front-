9207d34e8d853d6868354fda15b27cebc60fa4b6
Rojinhas <ruthrojasdasilva12@gmail.com>
2024-08-13 21:43:22 -0300

    48.2 - REACT-3 - Prática

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './pagina/home/Home';
+import Navbar from './components/footer/Footer';
+import Footer from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </>
+);
+}
+export default App;
