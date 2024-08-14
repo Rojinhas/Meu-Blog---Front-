2e6a2f1a83fb8966486d424fb652cad928072ff4
Rojinhas <ruthrojasdasilva12@gmail.com>
2024-08-14 16:13:01 -0300

    1.2 & 52.2 - REACT-6 - Prática

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,28 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pagina/login/Login';
 import Cadastro from './pagina/cadastro/Cadastro';
 import Home from './pagina/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/listaTemas';
 
 
 function App() {
   return (
     <>
-   
+    <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Cadastro />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-  
+        </AuthProvider>
     </>
   );
 }
